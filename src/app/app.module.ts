a6c85f77cd0651f8926297ce068deb63b73f4755
MarcA098 <168072534+MarcA098@users.noreply.github.com>
2024-11-29 15:25:01 +0100

    Borrar carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExempleComponent } from './Component exemple/exemple.component';
-import { RegistreUsuariComponent } from './registre-usuari/registre-usuari.component';
 import { RouterModule, Routes } from '@angular/router';
-import { ComponentHomeComponent } from './component-home/component-home.component';
 import { FormsModule } from '@angular/forms';
 import { ListadoEnfermerosComponent } from './listado-enfermeros/listado-enfermeros.component';
 
 const APP_ROUTES:Routes=[
   { path: '', component: ExempleComponent },
-  { path: 'registre', component: RegistreUsuariComponent },
-  { path: 'home', component: ComponentHomeComponent },
   {path: 'lista', component: ListadoEnfermerosComponent}
 ];
 
@@ -20,8 +16,6 @@ const APP_ROUTES:Routes=[
   declarations: [
     AppComponent,
     ExempleComponent,
-    RegistreUsuariComponent,
-    ComponentHomeComponent,
     ListadoEnfermerosComponent,
   ],
   imports: [
