95dac98aa69e526acabb2f937a654b4a896f3e89
MarcA098 <168072534+MarcA098@users.noreply.github.com>
2024-11-29 15:32:09 +0100

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegistreUsuariComponent } from './registre-usuari/registre-usuari.component';
-import { ComponentHomeComponent } from './component-home/component-home.component';
 import { ExempleComponent } from './Component exemple/exemple.component';
 import { ListadoEnfermerosComponent } from './listado-enfermeros/listado-enfermeros.component';
 
